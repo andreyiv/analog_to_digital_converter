7e886db73d49dd79deb52092bb0f1856956f0cd9
andreyiv <andrey.ivannikov@gmail.com>
2014-03-14 14:09:17 -0400

    Added argument parser

--- convert_a2d.py
+++ convert_a2d.py
@@ -2,24 +2,36 @@
 Provide analog waveform input, get digitial transitions as output.
 """
 
+import argparse
 import csv
 
-# TODO: Add an argument parser
-#     TODO: Add defaults to the values that are not required
+# TODO: Provide limits on ratios
 # TODO: Possibly move calculated values into an object
 # TODO: Break up code into functions
 # TODO: Refactor
 # TODO: Test
 
+# Create argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("input", help="path to input csv file of analog data")
+parser.add_argument("output", help="path to output csv file of digital transitions")
+parser.add_argument("-d", "--delimiter", help="specify delimiter used in input and output, ',' is used by default")
+parser.add_argument("-n", "--headers", type=int, help="number of header rows in input")
+parser.add_argument("-r", "--lratio", type=float, help="ratio of 1 to 0 crossing")
+parser.add_argument("-R", "--hratio", type=float, help="ratio of 0 to 1 crossing")
+
 def main():
 
+    # Parse arguments
+    input_arguments = parser.parse_args()
+
     # Collect input from user
-    input_file = args[1]
-    output_file = args[2]
-    delimiter = args[3]
-    num_of_header_lines = args[4]
-    high_threshold_ratio = args[5]
-    low_threshold_ratio = args[6]
+    input_file = input_arguments.input
+    output_file = input_arguments.output
+    delimiter = input_argumets.delimiter if input_arguments.delimiter else ','
+    num_of_header_lines = input_arguments.headers if input_arguments.headers else 1
+    low_threshold_ratio = input_arguments.lratio if input_arguments.lratio else 1/3
+    high_threshold_ratio = input_arguments.hratio if input_arguments.hratio else 2/3
 
     maximum = 0
     minimum = 0
@@ -75,6 +87,7 @@ def main():
         # Write initial row
         csv_output.writerow([row1[0], int(digital_value)])
 
+        # Write the rest of the rows in transitions
         for input_row in csv_input:
             if (input_row[1] > high_thresh and digital_value == False) or
                (input_row[1] < low_thresh and digital_value == True):
@@ -88,3 +101,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
