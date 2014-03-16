4b30fff3f609fed71952f8cca5c109eb454ecc5e
Andrey <andrey.ivannikov@gmail.com>
2014-03-16 14:06:48 -0400

    Bug fixes, added defaults to arguments

--- convert_a2d.py
+++ convert_a2d.py
@@ -4,6 +4,7 @@ Provide analog waveform input, get digitial transitions as output.
 
 import argparse
 import csv
+import pdb
 
 # TODO: Provide limits on ratios
 # TODO: Possibly move calculated values into an object
@@ -15,10 +16,10 @@ import csv
 parser = argparse.ArgumentParser()
 parser.add_argument("input", help="path to input csv file of analog data")
 parser.add_argument("output", help="path to output csv file of digital transitions")
-parser.add_argument("-d", "--delimiter", help="specify delimiter used in input and output, ',' is used by default")
-parser.add_argument("-n", "--headers", type=int, help="number of header rows in input")
-parser.add_argument("-r", "--lratio", type=float, help="ratio of 1 to 0 crossing")
-parser.add_argument("-R", "--hratio", type=float, help="ratio of 0 to 1 crossing")
+parser.add_argument("-d", "--delimiter", default=',', help="specify delimiter used in input and output, ',' is used by default")
+parser.add_argument("-n", "--headers", type=int, default=1, help="number of header rows in input")
+parser.add_argument("-r", "--lratio", type=float, default=1/3, help="ratio of 1 to 0 crossing")
+parser.add_argument("-R", "--hratio", type=float, default=2/3, help="ratio of 0 to 1 crossing")
 
 def main():
 
@@ -28,17 +29,16 @@ def main():
     # Collect input from user
     input_file = input_arguments.input
     output_file = input_arguments.output
-    delimiter = input_argumets.delimiter if input_arguments.delimiter else ','
-    num_of_header_lines = input_arguments.headers if input_arguments.headers else 1
-    low_threshold_ratio = input_arguments.lratio if input_arguments.lratio else 1/3
-    high_threshold_ratio = input_arguments.hratio if input_arguments.hratio else 2/3
+    delimiter = input_argumets.delimiter
+    num_of_header_lines = input_arguments.headers
+    low_threshold_ratio = input_arguments.lratio
+    high_threshold_ratio = input_arguments.hratio
 
     maximum = 0
     minimum = 0
 
     # Opening file once to read through
-    # with open(input_file, 'rb') as input_data:
-    with open(input_file, newline='') as input_data:
+    with open(input_file, mode='r', newline='') as input_data:
         csv_input = csv.reader(input_data, delimiter=delimiter) # Get csv reader
 
         # Ignoring header information
@@ -46,17 +46,18 @@ def main():
             next(csv_input)  # Throwing away header data
 
         # Initializing max and min
-        maximum = next(csv_input)[1]
+        maximum = float(next(csv_input)[1]) # TODO: provide a good error code for when it's not a float
         minimum = maximum
 
         # Finiding max and min
         for row in csv_input:
 
-            if maximum < row[1]:
-                maximum = row[1]
+            current_value = float(row[1])
+            if maximum < current_value:
+                maximum = current_value
 
-            if minimum > row[1]
-                minimum = row[1]
+            if minimum > current_value:
+                minimum = current_value
 
 
     # Calculate thresholds and middle analog value
@@ -67,20 +68,18 @@ def main():
 
 
     # Opening file one more time to read through
-    # with open(input_file, 'rb') as input_data
-         # open(output_file, 'rb') as output_data:
-    with open(input_file, newline='') as input_data
-         open(output_file, newline='') as output_data:
+    with open(input_file, mode='r', newline='') as input_data, \
+         open(output_file, mode='w', newline='') as output_data:
         csv_input = csv.reader(input_data, delimiter=delimiter) # Get csv reader
         csv_output = csv.writer(output_data, delimiter=delimiter) # Get csv writer
 
         # Funnel headers over to the output
         for i in range(num_of_header_lines):
-            csv_output.writerow(csv_input.next())
+            csv_output.writerow(next(csv_input))
 
         # Initialize values
-        row1 = csv_input.next()
-        analog_value = row1[1]
+        row1 = next(csv_input)
+        analog_value = float(row1[1])
         digital_value = False if analog_value < middle else True
         threshold_count = 0
 
@@ -89,11 +88,12 @@ def main():
 
         # Write the rest of the rows in transitions
         for input_row in csv_input:
-            if (input_row[1] > high_thresh and digital_value == False) or
-               (input_row[1] < low_thresh and digital_value == True):
+            current_value = float(input_row[1])
+            if (current_value > high_thresh and digital_value == False) or \
+               (current_value < low_thresh and digital_value == True):
 
                 digital_value = not digital_value
-                csv_output.writerow([row1[0], int(digital_value)])
+                csv_output.writerow([input_row[0], int(digital_value)])
                 threshold_count += 1
 
     print("Found {0} transitions in input file".format(threshold_count))
